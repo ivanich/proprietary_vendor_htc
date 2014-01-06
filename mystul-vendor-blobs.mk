e40a288c08da3ceb527075fe53c7231a19789856
Rob Shannon <Lloir@users.noreply.github.com>
2014-01-06 11:21:28 +0000

    Fixing up my mess

--- mystul-vendor-blobs.mk
+++ mystul-vendor-blobs.mk
@@ -59,18 +59,36 @@ PRODUCT_COPY_FILES += \
   vendor/htc/mystul/proprietary/bin/qmuxd:system/bin/qmuxd \
   vendor/htc/mystul/proprietary/bin/rmt_storage:system/bin/rmt_storage \
   vendor/htc/mystul/proprietary/bin/thermald:system/bin/thermald \
+  vendor/htc/mystul/proprietary/lib/egl/libEGL_adreno.so:system/vendor/lib/libEGL_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libq3dtools_adreno.so:system/vendor/lib/libq3dtools_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/eglsubAndroid.so:system/vendor/lib/eglsubAndroid.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/libGLESv2S3D_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv2_adreno.so:system/vendor/lib/libGLESv2_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/libGLESv1_CM_adreno.so \
+  vendor/htc/mystul/proprietary/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+  vendor/htc/mystul/proprietary/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
+  vendor/htc/mystul/proprietary/lib/libllvm-arm.so:system/vendor/lib/libllvm-arm.so \
+  vendor/htc/mystul/proprietary/lib/libc2d2_a3xx.so:system/vendor/lib/libc2d2_a3xx.so \
+  vendor/htc/mystul/proprietary/lib/libgsl.so:system/vendor/lib/libgsl.so \
+  vendor/htc/mystul/proprietary/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
+  vendor/htc/mystul/proprietary/lib/libCB.so:system/vendor/lib/libCB \
+  vendor/htc/mystul/proprietary/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
+  vendor/htc/mystul/proprietary/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
+  vendor/htc/mystul/proprietary/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+  vendor/htc/mystul/proprietary/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
+  vendor/htc/mystul/proprietary/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
+  vendor/htc/mystul/proprietary/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
+  vendor/htc/mystul/proprietary/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+  vendor/htc/mystul/proprietary/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
   vendor/htc/mystul/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
   vendor/htc/mystul/proprietary/lib/libHTC_DIS.so:system/lib/libHTC_DIS.so \
   vendor/htc/mystul/proprietary/lib/libcsd-client.so:system/lib/libcsd-client.so \
-  vendor/htc/mystul/proprietary/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
-  vendor/htc/mystul/proprietary/lib/libllvm-arm.so:system/vendor/lib/libllvm-arm.so \
   vendor/htc/mystul/proprietary/lib/libacdbmapper.so:system/lib/libacdbmapper.so \
   vendor/htc/mystul/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
   vendor/htc/mystul/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
-  vendor/htc/mystul/proprietary/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-  vendor/htc/mystul/proprietary/lib/libc2d2_a3xx.so:system/vendor/lib/libc2d2_a3xx.so \
-  vendor/htc/mystul/proprietary/lib/libc2d2_z180.so:system/vendor/lib/libc2d2_z180.so \
-  vendor/htc/mystul/proprietary/lib/libCB.so:system/vendor/lib/libCB \
   vendor/htc/mystul/proprietary/lib/libcam_oem_plugin.so:system/lib/libcam_oem_plugin.so \
   vendor/htc/mystul/proprietary/lib/hw/vendor-camera.msm8960.so:system/lib/hw/vendor-camera.msm8960.so \
   vendor/htc/mystul/proprietary/lib/libcameraasd.so:system/lib/libcameraasd.so \
@@ -98,19 +116,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/mystul/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
   vendor/htc/mystul/proprietary/lib/libqcci_legacy.so:system/lib/libqcci_legacy.so \
   vendor/htc/mystul/proprietary/lib/libqmi_client_qmux.so:system/lib/libqmi_client_qmux.so \
-  vendor/htc/mystul/proprietary/lib/egl/eglsubAndroid.so:system/vendor/lib/eglsubAndroid.so \
-  vendor/htc/mystul/proprietary/lib/egl/libEGL_adreno.so:system/vendor/lib/libEGL_adreno.so \
-  vendor/htc/mystul/proprietary/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/libGLESv1_CM_adreno.so \
-  vendor/htc/mystul/proprietary/lib/egl/libGLESv2_adreno.so:system/vendor/lib/libGLESv2_adreno.so \
-  vendor/htc/mystul/proprietary/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/libGLESv2S3D_adreno.so \
-  vendor/htc/mystul/proprietary/lib/egl/libq3dtools_adreno.so:system/vendor/lib/libq3dtools_adreno.so \
-  vendor/htc/mystul/proprietary/lib/libgsl.so:system/vendor/lib/libgsl.so \
-  vendor/htc/mystul/proprietary/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-  vendor/htc/mystul/proprietary/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
-  vendor/htc/mystul/proprietary/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-  vendor/htc/mystul/proprietary/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
-  vendor/htc/mystul/proprietary/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
-  vendor/htc/mystul/proprietary/lib/libc2d2_a3xx.so:system/vendor/lib/libc2d2_a3xx.so \
   vendor/htc/mystul/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
   vendor/htc/mystul/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
   vendor/htc/mystul/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
